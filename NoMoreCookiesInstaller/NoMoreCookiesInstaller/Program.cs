5ae726b7047327340dd1fd98de441fd6a2aa2010
AdvDebug <90452585+AdvDebug@users.noreply.github.com>
2023-07-01 18:17:59 +0300

    Add files via upload

--- NoMoreCookiesInstaller/NoMoreCookiesInstaller/Program.cs
+++ NoMoreCookiesInstaller/NoMoreCookiesInstaller/Program.cs
@@ -8,13 +8,15 @@ namespace NoMoreCookiesInstaller
     {
         static void Main(string[] args)
         {
-            Console.Write("Welcome to NoMoreCookies Installer!\n\n1. Install NoMoreCookies\n2. Uninstall NoMoreCookies\n\nOption: ");
+            Console.Write("Welcome to NoMoreCookies Installer!\n\n1. Install NoMoreCookies (Compatible with programs and games, only hooks Non-Signed Programs and Non-Services Processes)\n2. Install NoMoreCookies (Hooks all programs except services, also compatible with most games and software but may cause some delays, also it's experimental)\n\n3. Uninstall NoMoreCookies\n\nOption: ");
             string Option = Console.ReadLine();
             try
             {
                 string NoMoreCookiesx64 = Environment.CurrentDirectory + "\\NoMoreCookies_x64.dll";
                 string NoMoreCookiesx86 = Environment.CurrentDirectory + "\\NoMoreCookies.dll";
-                if (File.Exists(Environment.CurrentDirectory + "\\NoMoreCookies_x64.dll") && File.Exists(Environment.CurrentDirectory + "\\NoMoreCookies.dll"))
+                string XNoMoreCookiesx64 = Environment.CurrentDirectory + "\\XNoMoreCookies_x64.dll";
+                string XNoMoreCookiesx86 = Environment.CurrentDirectory + "\\XNoMoreCookies.dll";
+                if (File.Exists(Environment.CurrentDirectory + "\\NoMoreCookies_x64.dll") && File.Exists(Environment.CurrentDirectory + "\\NoMoreCookies.dll") && File.Exists(Environment.CurrentDirectory + "\\XNoMoreCookies_x64.dll") && File.Exists(Environment.CurrentDirectory + "\\XNoMoreCookies.dll"))
                 {
                     switch (Option)
                     {
@@ -61,6 +63,48 @@ namespace NoMoreCookiesInstaller
                             }
                             break;
                         case "2":
+                            if (!Environment.Is64BitOperatingSystem)
+                            {
+                                RegistryKey Local = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Windows", true);
+                                if (Local != null)
+                                {
+                                    Local.SetValue("AppInit_DLLs", "C:\\XNoMoreCookies.dll", RegistryValueKind.String);
+                                    Local.SetValue("LoadAppInit_DLLs", 1, RegistryValueKind.DWord);
+                                    File.Copy(XNoMoreCookiesx86, "C:\\XNoMoreCookies.dll");
+                                }
+                                else
+                                    Console.WriteLine("\n\nCouldn't open the AppInit_Dlls registry key for writing.");
+                                Console.WriteLine("\n\nSuccessfully installed XNoMoreCookies, please restart your system to apply changes.");
+                                Console.Read();
+                                Environment.Exit(0);
+                            }
+                            else
+                            {
+                                RegistryKey Local = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Windows", true);
+                                if (Local != null)
+                                {
+                                    Local.SetValue("AppInit_DLLs", "C:\\XNoMoreCookies_x64.dll", RegistryValueKind.String);
+                                    Local.SetValue("LoadAppInit_DLLs", 1, RegistryValueKind.DWord);
+                                    File.Copy(XNoMoreCookiesx64, "C:\\XNoMoreCookies_x64.dll");
+                                }
+                                else
+                                    Console.WriteLine("\n\nCouldn't open the AppInit_Dlls of x64 registry key for writing.");
+                                RegistryKey Local2 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+                                RegistryKey Local3 = Local2.OpenSubKey("SOFTWARE\\Wow6432Node\\Microsoft\\Windows NT\\CurrentVersion\\Windows", true);
+                                if (Local3 != null)
+                                {
+                                    Local3.SetValue("AppInit_DLLs", "C:\\XNoMoreCookies.dll", RegistryValueKind.String);
+                                    Local3.SetValue("LoadAppInit_DLLs", 1, RegistryValueKind.DWord);
+                                    File.Copy(XNoMoreCookiesx86, "C:\\XNoMoreCookies.dll");
+                                }
+                                else
+                                    Console.WriteLine("\n\nCouldn't open the AppInit_Dlls of x86 registry key for writing.");
+                                Console.WriteLine("\n\nSuccessfully installed XNoMoreCookies, please restart your system to apply changes.");
+                                Console.Read();
+                                Environment.Exit(0);
+                            }
+                            break;
+                        case "3":
                             if (!Environment.Is64BitOperatingSystem)
                             {
                                 RegistryKey Local3 = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Windows", true);
