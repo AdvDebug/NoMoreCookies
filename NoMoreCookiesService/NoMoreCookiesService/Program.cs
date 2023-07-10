f46cb2927e61fef1b059bc5021b5bdd6c51fbd8a
LinusTorvaldCoolMan <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-07-11 00:00:41 +0300

    Update Program.cs
    Commit Tray Icon

--- NoMoreCookiesService/NoMoreCookiesService/Program.cs
+++ NoMoreCookiesService/NoMoreCookiesService/Program.cs
@@ -1,4 +1,32 @@
-﻿
+
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NoMoreCookiesService
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Thread.Sleep(10000);
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new MainService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
+
+/*﻿
 using System;
 using System.Threading;
 using System.ServiceProcess;
@@ -52,4 +80,4 @@ namespace NoMoreCookiesService
            
         }
     }
-}
+} */
