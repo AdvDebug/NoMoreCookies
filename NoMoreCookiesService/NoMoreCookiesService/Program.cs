e99ebb136194ea948d61c8e2d0503263ef7e5adc
LinusTorvaldCoolMan <60674864+DiggerMan-cpp@users.noreply.github.com>
2023-07-10 23:48:27 +0300

    Add files via upload

--- NoMoreCookiesService/NoMoreCookiesService/Program.cs
+++ NoMoreCookiesService/NoMoreCookiesService/Program.cs
@@ -1,26 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.ServiceProcess;
-using System.Text;
+﻿
+using System;
 using System.Threading;
-using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Windows.Forms;
 
 namespace NoMoreCookiesService
 {
     internal static class Program
     {
+        private static NotifyIcon notifyIcon;
+
         static void Main()
         {
-            Thread.Sleep(10000);
+            //  NotifyIcon
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Text = "NoMoreCookiesService";
+            notifyIcon.Visible = true;
+
+            // context NotifyIcon
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit");
+            exitMenuItem.Click += ExitMenuItem_Click;
+            contextMenu.Items.Add(exitMenuItem);
+            notifyIcon.ContextMenuStrip = contextMenu;
+
+            // double click
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+
+            // start service
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
                 new MainService()
             };
             ServiceBase.Run(ServicesToRun);
+
+            // Infinity time
+            Application.Run();
+        }
+
+        private static void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            // Exit
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            Application.Exit();
+        }
+
+        private static void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+           
         }
     }
 }
